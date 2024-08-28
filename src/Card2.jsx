44c2ba9fe972ef56171f355cd21d5f1c57f51e87
Kartikkumaria2 <100405990+Kartikkumaria2@users.noreply.github.com>
2024-08-28 07:15:52 +0530

    Update Card2.jsx

--- src/Card2.jsx
+++ src/Card2.jsx
@@ -3,12 +3,12 @@ import { useEffect, useState } from 'react';
 
 function Card2({ cart, setCart }) {
   const [data, setData] = useState([]);
-  const [displayed, setDisplayed] = useState([]);
-  const [length, setLength] = useState([]);
+  const [displayed, setDisplayed] = useState({});
+  const [length, setLength] = useState({});
   const [totalPrice, setTotalPrice] = useState(0);
 
   useEffect(() => {
-    async function getProductData() {
+    async function getProductDataAndCalculateTotal() {
       const fetchData = async (element) => {
         const response = await fetch(`https://fakestoreapi.com/products/${element}`);
         return response.json();
@@ -19,66 +19,39 @@ function Card2({ cart, setCart }) {
       try {
         const productData = await Promise.all(promises);
         setData(productData);
+
+        // Calculate the total price within the same hook
+        const total = productData.reduce((acc, item) => {
+          const count = cart.filter((id) => id === item.id.toString()).length;
+          return acc + item.price * count;
+        }, 0);
+        setTotalPrice(total);
+
+        // Update displayed items and lengths
+        const occurrences = {};
+        const lengthOccurrences = {};
+
+        cart.forEach((element) => {
+          productData.forEach((ele) => {
+            if (ele.id === parseInt(element)) {
+              occurrences[ele.id] = {
+                image: ele.image,
+                title: ele.title,
+              };
+              lengthOccurrences[ele.id] = cart.filter((item) => item === element).length;
+            }
+          });
+        });
+
+        setDisplayed(occurrences);
+        setLength(lengthOccurrences);
       } catch (error) {
         console.error('Error fetching product data:', error);
       }
     }
 
-    getProductData();
+    getProductDataAndCalculateTotal();
   }, [cart]);
-
-  function filler2() {
-    const occurrences = {};
-    cart.forEach((element) => {
-      data.forEach((ele) => {
-        if (ele["id"] == element) {
-          occurrences[ele["id"]] = {
-            image: ele["image"],
-            title: ele["title"]
-          };
-        }
-      });
-    });
-
-    return occurrences;
-  }
-
-  useEffect(() => {
-    async function updatedData() {
-      let occurrences = filler2();
-      setDisplayed(occurrences);
-    }
-    updatedData();
-  }, [data]);
-
-  function filler() {
-    const occurrences = {};
-    cart.forEach((element) => {
-      data.forEach((ele) => {
-        if (ele["id"] == element) {
-          occurrences[ele["id"]] = cart.filter((item) => item === element).length;
-        }
-      });
-    });
-
-    return occurrences;
-  }
-
-  useEffect(() => {
-    let occurrences = filler();
-    setLength(occurrences);
-  }, [data]);
-
-  useEffect(() => {
-    const calculateTotalPrice = () => {
-      const total = data.reduce((acc, item) => {
-        const count = cart.filter((id) => id === item.id).length;
-        return acc + item.price * count;
-      }, 0);
-      setTotalPrice(total);
-    };
-    calculateTotalPrice();
-  }, [cart, data]);
 
   const plusClick = function (e) {
     e.preventDefault();
@@ -90,20 +63,16 @@ function Card2({ cart, setCart }) {
     let id = e.target.getAttribute('attri');
     const itemIndex = cart.findIndex((item) => item === id);
 
-    // If the item exists, decrement its count or remove it if count becomes zero
     if (itemIndex !== -1) {
       const updatedCart = [...cart];
 
       if (updatedCart.filter((item) => item === id).length > 1) {
-        // If the item count is greater than 1, decrement its count
         const indexOfId = updatedCart.indexOf(id);
         updatedCart.splice(indexOfId, 1);
       } else {
-        // If the item count is 1, remove the item from the cart
         updatedCart.splice(itemIndex, 1);
       }
 
-      // Update the cart state
       setCart(updatedCart);
     }
   };
@@ -131,8 +100,8 @@ function Card2({ cart, setCart }) {
 }
 
 Card2.propTypes = {
-  cart: PropTypes.array,
-  setCart: PropTypes.func,
+  cart: PropTypes.array.isRequired,
+  setCart: PropTypes.func.isRequired,
 };
 
 export default Card2;
